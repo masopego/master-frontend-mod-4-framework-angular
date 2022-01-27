cdf18254299dd21637ece73c5aa293ecb19d60af
Marisol Pena <masopego@gmail.com>
2022-01-27 15:31:49 +0100

    Add routes

--- mini-app/src/app/app-routing.module.ts
+++ mini-app/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about_us', component: AboutUsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
