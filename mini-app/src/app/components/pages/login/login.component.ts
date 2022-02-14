6fcfc5d6d30128e3f63d2642752942be3d8565c2
Marisol Pena <masopego@gmail.com>
2022-02-14 13:38:54 +0100

    Add login page

--- mini-app/src/app/components/pages/login/login.component.ts
+++ mini-app/src/app/components/pages/login/login.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  userNameControl!: FormControl;
+  passwordControl!: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.createLoginForm();
   }
 
+  createLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+
+    this.userNameControl = this.loginForm.get('userName') as FormControl;
+    this.passwordControl = this.loginForm.get('password') as FormControl;
+  }
+
+  resetForm(): void {
+    this.loginForm.reset();
+  }
+
+  handleLogin(): boolean {
+    let newLogin = this.loginForm?.value;
+    const isLoginOk = this.authService.login(newLogin);
+
+    if (!isLoginOk) {
+      this.resetForm();
+      alert('Login no válido. Inténtalo de nuevo');
+    }
+
+    return isLoginOk;
+  }
+
+  ngOnInit(): void {}
 }
