0d86b1d0933816996d619137a796dccbc07b2897
Marisol Pena <masopego@gmail.com>
2022-02-14 13:39:39 +0100

    Add disabled state to button component

--- mini-app/src/app/components/atoms/button/button.component.ts
+++ mini-app/src/app/components/atoms/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() children: string | undefined;
+  @Input() disabled: boolean | undefined;
 
   constructor() {}
 
