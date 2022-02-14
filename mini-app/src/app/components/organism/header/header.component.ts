414288ae9c81cb375a33dc17d3b1826830e2c5b8
Marisol Pena <masopego@gmail.com>
2022-02-14 15:47:50 +0100

    Add islogged method to header

--- mini-app/src/app/components/organism/header/header.component.ts
+++ mini-app/src/app/components/organism/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  isLogin: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.isLogin = false;
   }
 
+  handlePrivateRoute(): void {
+    this.isLogin = this.authService.isLogged();
+    console.log(this.isLogin);
+  }
+
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    this.handlePrivateRoute();
+  }
 }
